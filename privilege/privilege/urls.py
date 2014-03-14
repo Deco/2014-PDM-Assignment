410d5a3a17d9957bd80b997184969e2384e546a8
Mike Aldred <mike.aldred@gmail.com>
2014-03-13 16:36:53 -0800

    Removed all PEP8 and Pylint warnings.

--- privilege/privilege/urls.py
+++ privilege/privilege/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     url(r'^$', 'core.views.home', name='home'),
     # url(r'^privilege/', include('privilege.foo.urls')),
